1aab6c933eaa2620444feb316bf6d95eae2c34a4
Reel48 <admin@reel48.com>
2025-06-18 20:19:17 -0500

    Show Navbar, HeroSection, FaqSection, and FooterSection on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import FaqSection from './components/FaqSection';
+import FooterSection from './components/FooterSection';
 import NotFound from './pages/NotFound';
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <FaqSection />
+      <FooterSection />
+    </>
+  );
+}
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>

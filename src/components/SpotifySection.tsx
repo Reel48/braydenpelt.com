e345653ad62309cd80dd8a0638a59f8e3b10a9f5
Reel48 <admin@reel48.com>
2025-06-23 22:25:23 -0500

    All three Spotify playlists now use iframe embed code as requested

--- src/components/SpotifySection.tsx
+++ src/components/SpotifySection.tsx
@@ -16,9 +16,39 @@ const SpotifySection = () => {
           <img src="/spotify_logo_pelt.png" alt="Spotify Logo Pelt" className="h-24" />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 place-items-center">
-          {playlistUrls.map((url, index) => (
-            <Spotify key={index} link={url} />
-          ))}
+          <iframe
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/playlist/4TfPPw4bFSdS8wll6PY5rW?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+            title="Brayden's Playlist"
+          ></iframe>
+          <iframe
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/playlist/0sgca4DLP195nWzRbkN8ZB?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+            title="Brayden's Alternate Playlist"
+          ></iframe>
+          <iframe
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/playlist/0eYoFYhpqZ3flBUdCfaKAY?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+            title="Old Music Playlist"
+          ></iframe>
         </div>
       </div>
     </section>

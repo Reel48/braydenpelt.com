5fc27a1eec72399fca3e100817ba85e28937a4b5
Reel48 <admin@reel48.com>
2025-06-24 16:07:17 -0500

    Update Movies section header image to movies_logo.png

--- src/components/MoviesSection.tsx
+++ src/components/MoviesSection.tsx
@@ -20,7 +20,7 @@ const MoviesSection = () => {
     <section style={{ backgroundColor: 'var(--color-tan)' }} className="py-20">
       <div className="container mx-auto px-4">
         <div className="flex justify-center mb-12">
-          <img src="/letterboxd-logo.png" alt="Letterboxd Logo" className="h-24" />
+          <img src="/movies_logo.png" alt="Movies Logo" className="h-24" />
         </div>
         <div id="letterboxd-embed-wrapper-tc" className="embed-green-bg">Loading...</div>
       </div>
